3119ac7dc2b170e8d91a78f66e43beead72321d6
claudialegrec <60103864+claudialegrec@users.noreply.github.com>
2021-11-11 13:33:54 -0700

    First commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <navbar/>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import navbar from "./components/Navbar"
+
+export default {
+  components: {
+    navbar,
+  }
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+@import'~bootstrap/dist/css/bootstrap.css'
+</style>
