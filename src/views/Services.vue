0c4a8140869459aaa8e5c6e88c8e6a98a7f1df1d
claudialegrec <60103864+claudialegrec@users.noreply.github.com>
2021-11-11 17:37:33 -0700

    Router funcional, se esta trabajando con componente de lista de servicios

--- src/views/Services.vue
+++ src/views/Services.vue
@@ -1,12 +1,23 @@
 <template>
+  <div class="bg-danger" style="padding:30px">
+    <h1 style="margin-bottom:30px">Aqui se muestran los servicios</h1>
+
     <div>
-        <p>Aqui se muestran los servicios</p>
-        <ul>
-            <li>Servicio 1</li>
-            <li>Servicio 2</li>
-            <li>Servicio 3</li>
-        </ul>
+      <h3>Servicio 1</h3>
+      <ul>
+        <li><b>Nombre del servicio:</b> Servicio 1</li>
+        <li><b>Descirpción:</b> 
+        Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor 
+        incididunt ut labore et dolore magna aliqua.
+        Ut enim ad minim veniam, quis nostrud exercitation 
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.</li>
+      </ul>
     </div>
+
+    <button class="btn btn-primary">Editar servicios</button>
+
+  </div>
 </template>
 
 <script>
